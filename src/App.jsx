3a6991572a7178d0a05c73c7095292694d38730f
IliasSari <sarigilias@gmail.com>
2026-06-22 21:03:39 +0300

    Second commit. Added games file!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import Games from './Games.jsx';
 
 function App(){
   return(
     <>
       <Header/>   
+      <Games/>
       <Footer/>
     </>
   );
---
+++ src/Games.jsx
@@ -0,0 +1,15 @@
+
+function Games(){
+    
+    const games1 = "Assasins Creed"
+    const games2 = "Prince of Persia"
+    return(
+        <ul>
+            <li>Need for speed</li>
+            <li>{games1}</li>
+            <li>{games2.toUpperCase}</li>
+        </ul>
+    );
+}
+
+export default Games;
