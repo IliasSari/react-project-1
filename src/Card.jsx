9b1512bd865794f97df94375fdc41326b8727b30
IliasSari <sarigilias@gmail.com>
2026-06-27 15:52:22 +0300

    4th Commit, just added some card components

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,9 +3,9 @@ import profilePic from './assets/games.jpg'
 function Card(){
     return(
         <div className="card">
-            <img src={profilePic} alt="profile picture"></img>
-            <h2>Ilias</h2>
-            <p>I want to learn React</p>
+            <img className = "card-image" src={profilePic} alt="profile picture"></img>
+            <h2 className = "card-title">Ilias</h2>
+            <p className = "card-text">I want to learn React</p>
         </div>
     );
 }
