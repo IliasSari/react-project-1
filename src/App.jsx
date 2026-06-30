fdc725645b44cdfe335edfb51f68a78449683e08
IliasSari <sarigilias@gmail.com>
2026-06-30 17:30:57 +0300

    5th commit. Added Click Me buttongit add .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Games from './Games.jsx';
 import Card from './Card.jsx';
+import Button from './Button.jsx';
 
 function App(){
   return(
@@ -10,6 +11,7 @@ function App(){
       <Games/>
       <Footer/>
       <Card/>
+      <Button/>
     </>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,18 @@
+
+
+function Button(){
+    
+    const styles = {
+
+        backgroundColor: "rgb(39, 69, 89)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "4px",
+        border: "none",
+        cursor: "pointer",
+    }
+
+    return(<button style={styles}>Click me</button>);
+}
+
+export default Button
