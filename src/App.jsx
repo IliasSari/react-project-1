18e6b0b0d067057a1ad8f3f7c823f4a822ae5f2e
IliasSari <sarigilias@gmail.com>
2026-07-11 14:52:18 +0300

    made render list

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,15 @@ import UserGreeting from './UserGreeting.jsx';
 import List from './List.jsx';
 
 function App(){
+        const fruits = [{id: 1, name: "apple", calories: 95},
+                  {id: 2, name: "banana", calories: 105},
+                  {id: 3, name: "orange", calories: 45},
+                  {id: 4, name: "coconut", calories: 159},
+                  {id: 5, name: "pinaple", calories: 37}
+              ];
   return(
     <>
+
       <Header/>   
       <Games/>
       <Footer/>
@@ -18,8 +25,8 @@ function App(){
       <Students name="George" age="27" isStudents={true}/>
       <Students name="John" age="34" isStudents={false}/>
       <Students name="Bill" age="28" isStudents={true}/>
-      <UserGreeting isLoggedIn={true} username="Ilias"/>
-      <List/>
+      <UserGreeting isLoggedIn={true} username="Ilias"/> 
+      <List items={fruits} category={fruits}/>
     </>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,13 @@
 
-function List(){
-    const fruits = ["apple","banana","orange", "coconut", "pinaple"]
+function List(props){
+    
+    const itemList = props.items;
+    // fruits.sort((a,b) => a.name.localeCompare(b.name));
+    const listItems = itemList.map(fruit => <li key={fruit.id}>
+                                        {fruit.name}: &nbsp;
+                                        <b>{fruit.calories}</b></li>);
 
-    return(fruits);
-}
+    return(<ol>{listItems}</ol>);
+}   
 
 export default List
