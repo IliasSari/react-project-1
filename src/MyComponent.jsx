5e09ff8cf4d86babc07c80400d60b9c497c92e2d
IliasSari <sarigilias@gmail.com>
2026-07-13 23:25:18 +0300

    Added 2 more buttons in this Component

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,17 +1,32 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 
 
 function MyComponent(){
 
     const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName ("George");
-        
+    }
+
+    const incementAge = () => {
+        setAge(age + 1);
+    }
+
+    const toggleEmployedStatus = () => {
+        setIsEmployed(!isEmployed);
     }
     return(<div>
                 <p>Name: {name}</p>
                 <button onClick={updateName}>Set Name</button>
+
+                <p>Age: {age}</p>
+                <button onClick={incementAge}>Incement Age</button>
+
+                <p>isEmployed: {isEmployed ? "Yes" : "No"}</p>
+                <button onClick={toggleEmployedStatus}>Incement Age</button>
     </div>);
     
 }
