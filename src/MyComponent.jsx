ae287051e5aa74fd9fd42482aaf4404c8f262a05
IliasSari <sarigilias@gmail.com>
2026-07-18 22:44:51 +0300

    FInished with event handlergit add . Just added some more!

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,4 +1,4 @@
-import {use, useState} from 'react';
+import {useState} from 'react';
 
 
 function MyComponent(){
@@ -6,6 +6,8 @@ function MyComponent(){
    const [name,setName] = useState("Guest");
    const [quantity, setQuantity] = useState(1);
    const [comment, setComment] = useState("");
+   const [payment, setPayment] = useState("");
+   const [shipping, setShipping] = useState("");
    
    function handleNameChange(event){
     setName(event.target.value);
@@ -18,6 +20,15 @@ function MyComponent(){
    function handleCommentChange(event){
     setComment(event.target.value);
    }
+
+   function handlePaymentChange(event){
+    setPayment(event.target.value);
+   }
+
+   function handleShippingChange(event){
+    setShipping(event.target.value);
+   }
+
    return(<div>
         <input value={name} onChange={handleNameChange}/>
         <p>Name: {name}</p>
@@ -27,6 +38,25 @@ function MyComponent(){
 
         <textarea value={comment} onChange={handleCommentChange} placeholder='Enter comments'/>
         <p>Comment: {comment}</p>
+
+        <select value={payment} onChange={handlePaymentChange}>
+            <option value="">Select an option</option>
+            <option value="Visa">Visa</option>
+            <option value="MasterCard">MasterCard</option>
+            <option value="GiftCard">GiftCard</option>
+        </select>
+        <p>Payment: {payment}</p>
+
+        <label>
+            <input type="radio" value="Pick up" checked={shipping === "Pick up"}
+            onChange={handleShippingChange}/>Pick up
+        </label>
+        <label>
+            <input type="radio" value="Delivery" checked={shipping === "Delivery"}
+            onChange={handleShippingChange}/>
+            Delivery
+        </label>
+        <p>Shipping: {shipping}</p>
    </div>)
     
 }
