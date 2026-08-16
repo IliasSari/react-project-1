932191fa716009b6d5e3cd28b814a0abe91e0752
IliasSari <sarigilias@gmail.com>
2026-08-16 21:41:01 +0300

    FInished with useContext

--- src/ComponentA.jsx
+++ src/ComponentA.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import ComponentB from "./ComponentB";
+
+export const UserContext = createContext();
 
 function ComponentA(){
 
-    const [user, setUser] = useState("Ilias")
+    const [user, setUser] = useState("Ilias");
 
     return(
         <div className="box">
             <h1>ComponentA</h1>
             <h2>{`Hello ${user}`}</h2>
-            <ComponentB user ={user}/>
+            <UserContext.Provider value = {user}>
+
+                <ComponentB user ={user}/>
+            </UserContext.Provider>
         </div>
     );
 
